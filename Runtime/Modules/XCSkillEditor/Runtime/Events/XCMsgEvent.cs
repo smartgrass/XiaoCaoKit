dc0f298972d7f5709b1f57d07e20910aca776b0b
smartgrass <30657539+smartgrass@users.noreply.github.com>
2024-02-27 00:16:53 +0800

    1

--- Runtime/Modules/XCSkillEditor/Runtime/Events/XCMsgEvent.cs
+++ Runtime/Modules/XCSkillEditor/Runtime/Events/XCMsgEvent.cs
@@ -16,53 +16,17 @@ namespace XiaoCao
             {
                 task.SetFinish();
             }
-            else if (msgType is EntityMsgType.InputCheck)
-            {
-                //this.task.CheckInput()
-            }
             baseMsg.state = 0;
             Info.role.ReceiveMsg(msgType, Info.entityId, baseMsg);
         }
 
-        public override void OnUpdateEvent(int frame, float timeSinceTrigger)
-        {
-            if (msgType is EntityMsgType.InputCheck)
-            {
-                Player0 player0 = task.Info.role as Player0;
 
-                if (player0.playerData.inputData.inputs[InputKey.NorAck])
-                {
-                    DebugGUI.Debug("Mouse1", $"---  Input Mouse1");
-
-                    baseMsg.state = 1;
-                }
-
-                //if (Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.J))
-                //{
-                //   DebugGUI.Debug("Mouse0",$"---  Input Mouse0");
-                //}
-                //Player0 player0 = this.Info.role as Player0;
-                ////this.task.CheckInput()
-                //if (player0 != null)
-                //{
-                //    player0.playerData.inputData.CheckKeyCode
-                //}
-            }
-
-        }
 
         public override void OnFinish()
         {
             base.OnFinish();
             if (HasTrigger && isUndoOnFinish)
             {
-                if (msgType is EntityMsgType.InputCheck)
-                {
-                    if (baseMsg.numMsg == 1)
-                    {
-
-                    }
-                }
                 baseMsg.state = 1;
                 Info.role.ReceiveMsg(msgType, Info.entityId, baseMsg);
             }
